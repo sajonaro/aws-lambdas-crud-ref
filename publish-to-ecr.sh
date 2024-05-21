13b4b05d9856ec56b6b76c3ce255e3fdcc54b084
sajonaro <alex.kunich@gmail.com>
2024-05-21 15:29:33 +0300

    updated scripts

--- publish-to-ecr.sh
+++ publish-to-ecr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # awscli needs to installed and configured, see https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 628654266155.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 628654266155.dkr.ecr.us-east-1.amazonaws.com
 
 docker build -t php-app-rep .
 
